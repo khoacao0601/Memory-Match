d56efe450b2fc3c33d19c5f1a595e5b33ca9debc
Khoa Cao <khoacao0601@gmail.com>
2020-09-18 23:39:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function welcomeBox() //Modal Box for Welcome Box
     {
         var welcome = $(".welcome");
         var submit = $(".submit");
-0123456789
+
         welcome.show();
 
         submit.click(function(){
